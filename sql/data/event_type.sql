afa39fdf4858212ee4a47e9bb510cf5f7c8347f4
Matthew Hood <hmshood@gmail.com>
2012-09-13 20:23:33 +0100

    Change event types to ones which occur at conventions rather than conferences

--- sql/data/event_type.sql
+++ sql/data/event_type.sql
@@ -3,10 +3,11 @@ SET client_encoding = 'UTF8';
 SET search_path = public, pg_catalog;
 
 INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('contest', true, NULL);
+INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('dance', true, NULL);
 INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('lecture', true, NULL);
 INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('lightning', true, NULL);
 INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('meeting', true, NULL);
 INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('movie', true, NULL);
 INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('other', true, NULL);
-INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('podium', true, NULL);
+INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('setup', false, NULL);
 INSERT INTO event_type (event_type, public_role_required, rank) VALUES ('workshop', true, NULL);
